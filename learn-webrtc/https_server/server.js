3fc6720b61dedebda432c1e7e855a5d3c12b8963
skr <bvlcsae@gmail.com>
2021-08-12 00:22:06 +0800

    feat

---
+++ learn-webrtc/https_server/server.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
+var serveIndex = require('serve-index');
+
+var express = require('express');
+var app = express();
+
+//顺序不能换
+app.use(serveIndex('./public'));
+app.use(express.static('./public'));
+
+var options = {
+    key  : fs.readFileSync('./cert/6082957_www.clearlove520.com.key'),
+    cert : fs.readFileSync('./cert/6082957_www.clearlove520.com.pem')
+}
+
+var https_server = https.createServer(options, app);
+https_server.listen(443, '0.0.0.0');
+
+
